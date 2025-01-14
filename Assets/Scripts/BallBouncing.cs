760f3412a2ad9be5a2894110bec317aabd9d6b86
dylan2766 <reevesdy@sheridancollege.ca>
2025-01-14 17:18:27 -0500

    - Working on Part 4 of gym

--- Assets/Scripts/BallBouncing.cs
+++ Assets/Scripts/BallBouncing.cs
@@ -16,15 +16,15 @@ public class BallBouncing : MonoBehaviour
     void Update()
     {
         Vector2 ballPos = transform.position;
-        ballPos.x += ballSpeedX;
-        ballPos.y += ballSpeedY;
+        ballPos.x += ballSpeedX * Time.deltaTime;
+        ballPos.y += ballSpeedY * Time.deltaTime;
 
         //Ball bouncing
         transform.Translate(ballPos.x * Time.deltaTime ,ballPos.y * Time.deltaTime ,0);
 
         //Testing Camera Borders
         Vector2 screenSize = new Vector2(Screen.width, Screen.height);
-        Vector2 screenSizeWorld = new Vector2();
+        Vector2 screenSizeWorld = new Vector2(Screen.width, Screen.height);
         screenSizeWorld = Camera.main.ScreenToWorldPoint(screenSize);
 
         Vector2 screenZeroWorld = Camera.main.ScreenToWorldPoint(Vector2.zero);
