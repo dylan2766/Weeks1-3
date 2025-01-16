77306b90e901c8aa9ff660ec5b5aa61ed8d5b847
dylan2766 <reevesdy@sheridancollege.ca>
2025-01-15 22:08:51 -0500

    - Added dolphin rotation to where mouse is click and it resets rotation and a certain angle

---
+++ Assets/Mechanical Drawing Assignment/Scripts/MoveDolphin.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDolphin : MonoBehaviour
+{
+    //speed variable
+    public float rotSpeed;
+
+    //Set dolphin y position
+    public float PosY;
+
+    //Mouse position variable
+    public Vector2 mousePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //When mouse clicked moves rotation point of dolphin to x position where mouse clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.y = PosY;
+            transform.position = mousePos;
+            transform.localEulerAngles = Vector3.zero;
+        }
+        
+        //When dolphin reaches an angle that is less than or equal to 180 it resets angle to 0
+        if (transform.localEulerAngles.z <= 160)
+        {
+            transform.localEulerAngles = Vector3.zero;
+        }
+
+        //Rotates the dolphin
+        transform.Rotate(0, 0, rotSpeed * Time.deltaTime);
+    }
+}
