3f83c03d6b6dd921f19d356b2f863c34cf0b50fe
dylan2766 <reevesdy@sheridancollege.ca>
2025-01-15 22:41:49 -0500

    - Changed dolphin to move to the mouse position when it reaches a certain angle

--- Assets/Mechanical Drawing Assignment/Scripts/MoveDolphin.cs
+++ Assets/Mechanical Drawing Assignment/Scripts/MoveDolphin.cs
@@ -22,22 +22,25 @@ public class MoveDolphin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //When mouse clicked moves rotation point of dolphin to x position where mouse clicked
-        if (Input.GetMouseButtonDown(0))
+        ////When mouse clicked moves rotation point of dolphin to x position where mouse clicked
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //    mousePos.y = PosY;
+        //    transform.position = mousePos;
+        //    transform.localEulerAngles = Vector3.zero;
+        //}
+        
+        //When dolphin reaches an angle that is less than or equal to 180 it resets angle to 0. Dolphins X axis is moved to where the mousePosition is.
+        if (transform.localEulerAngles.z < 180)
         {
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePos.y = PosY;
             transform.position = mousePos;
             transform.localEulerAngles = Vector3.zero;
         }
-        
-        //When dolphin reaches an angle that is less than or equal to 180 it resets angle to 0
-        if (transform.localEulerAngles.z <= 160)
-        {
-            transform.localEulerAngles = Vector3.zero;
-        }
 
         //Rotates the dolphin
-        transform.Rotate(0, 0, rotSpeed * Time.deltaTime);
+        transform.Rotate(0, 0, -rotSpeed * Time.deltaTime);
     }
 }
