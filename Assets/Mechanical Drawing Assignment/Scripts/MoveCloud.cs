67df70d30322e38ccf68a61238f40337b80ce4a9
dylan2766 <reevesdy@sheridancollege.ca>
2025-01-25 11:54:11 -0500

    - Added sky and clouds that move right to left

---
+++ Assets/Mechanical Drawing Assignment/Scripts/MoveCloud.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCloud : MonoBehaviour
+{
+    //public variables
+    public float speed;
+    public float startPos;
+
+    public AnimationCurve cloudCurve;
+    [Range(0, 1)] public float cloudPos;
+
+    public Transform start;
+    public Transform end;
+
+    //private variables
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cloudPos = startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move cloud to the left of the screen
+        cloudPos += speed * Time.deltaTime;
+
+        if (cloudPos >= 1)
+        {
+            cloudPos = 0;
+        }
+
+        transform.position = Vector2.Lerp(start.position,end.position, cloudCurve.Evaluate(cloudPos));
+    }
+}
